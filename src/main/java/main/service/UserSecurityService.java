66513e23ef98bfd05a765142e0995bec74db4788
GutorovMikhail <mgutorov@phoenixit.ru>
2024-01-22 15:11:33 +0300

    Удаляем пока тесты вообще.

--- src/main/java/main/service/UserSecurityService.java
+++ src/main/java/main/service/UserSecurityService.java
@@ -1,11 +1,12 @@
 package main.service;
 
-import lombok.AllArgsConstructor;
-import main.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import main.repository.UserRepository;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +16,6 @@ public class UserSecurityService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+        return userRepository.findByUsername(username).orElse(null);
     }
 }
